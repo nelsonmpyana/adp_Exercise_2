c99fceaf3a0270dd0101acfbb9889e818d096316
sizwe.ncikana@gmail.com <japhetmibatu1@gmail.com>
2020-02-24 15:46:31 +0200

    Second Commit

--- src/test/java/org/example/AppTestTest.java
+++ src/test/java/org/example/AppTestTest.java
@@ -1,21 +1,36 @@
 package org.example;
+import org.junit.Rule;
 import org.junit.jupiter.api.BeforeEach;
 import  org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
+import org.junit.rules.Timeout;
 
 import static org.junit.Assert.*;
 
 public class AppTestTest {
 
+    //failed test
     @Test
-    public void test() {
+    public void testaddPass() {
 
         runCalculate test =new runCalculate();
         int result= test.addNumber(5,8);
-        assertEquals(12,result);
+        assertEquals(13,result);
 
     }
+    @Test
+    public void testAddFail () {
+
+        runCalculate failtest = new runCalculate();
+        int results = failtest.addNumber(5, 8);
+        assertEquals(12, results);
+    }
+
+    //timeout test
+
+    @Test
     public void testTimeOut() {
+
     }
 
 
