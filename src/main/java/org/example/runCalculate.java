f58745f0d971180883bd32207dd0a9fd73121d93
Nelson <nelsonmpyana@gmail.com>
2020-02-26 08:04:59 +0200

    added additional test cases

--- src/main/java/org/example/runCalculate.java
+++ src/main/java/org/example/runCalculate.java
@@ -5,4 +5,10 @@ public class runCalculate {
     public int addNumber(int a,int b) {
         return a+b;
     }
+
+    public int sub(int x, int y) { return x-y;};
+    public int dev (int x, int y) { if (y==0) {throw new ArithmeticException();}
+    return x/y; }
 }
+
+
--- src/test/java/org/example/AppTestTest.java
+++ src/test/java/org/example/AppTestTest.java
@@ -1,9 +1,15 @@
 package org.example;
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Rule;
+import org.junit.*;
 import org.junit.jupiter.api.BeforeEach;
 import  org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
+import org.junit.rules.Timeout;
 
 import static org.junit.Assert.*;
+
 
 public class AppTestTest {
 
@@ -15,8 +21,23 @@ public class AppTestTest {
         assertEquals(12,result);
 
     }
-    public void testTimeOut() {
+
+    @Ignore
+    @Test
+    public void testSub() {
+        runCalculate test =new runCalculate();
+        int x=10;
+        int y =20;
+        assertEquals(-10, test.sub(x,y));
     }
 
+
+    @Ignore
+    @Test(expected=ArithmeticException.class, timeout=2000)
+    public void dev () {
+        runCalculate test =new runCalculate();
+        Assert.assertEquals(2,test.dev(4,0));
+
+    }
 
 }
